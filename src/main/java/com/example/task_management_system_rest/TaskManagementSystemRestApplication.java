7af1091e2988ca5298af3fe0bd9091be764a43cc
ArtavazdDeveloper <artavazd1993@gmail.com>
2024-11-21 20:36:41 +0300

    Initial commit

---
+++ src/main/java/com/example/task_management_system_rest/TaskManagementSystemRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.task_management_system_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementSystemRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagementSystemRestApplication.class, args);
+    }
+
+}
